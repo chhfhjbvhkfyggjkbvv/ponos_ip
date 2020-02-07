e16d810666aa4127341f2c304a145654542486b7
chhfhjbvhkfyggjkbvv <chhfhjbvhkfyggjkbvv@gmail.com>
2020-02-07 15:20:50 +0100

    vendredi 7 février 2020, 15:20:50 (UTC+0100)

--- ponos_ip.sh
+++ ponos_ip.sh
@@ -8,3 +8,5 @@ git config user.email "chhfhjbvhkfyggjkbvv@gmail.com"
 git add --all
 git commit -am "$(date)"
 git push origin --all
+echo "expect \"Username for 'https://github.com':\""
+echo "send -- chhfhjbvhkfyggjkbvv@gmail.com"
