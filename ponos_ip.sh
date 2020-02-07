e9b9abbe649ad71141a84fc5f88f25a74151f68d
chhfhjbvhkfyggjkbvv <chhfhjbvhkfyggjkbvv@gmail.com>
2020-02-07 15:12:52 +0100

    vendredi 7 février 2020, 15:12:52 (UTC+0100)

--- ponos_ip.sh
+++ ponos_ip.sh
@@ -4,4 +4,5 @@ ponos_ip=$(curl -s ip.engles.fr)
 echo -n "${ponos_ip}" > ./ip
 git add --all
 git config user.name "chhfhjbvhkfyggjkbvv"
+git config user.email "chhfhjbvhkfyggjkbvv@gmail.com"
 git commit -am "$(date)"
