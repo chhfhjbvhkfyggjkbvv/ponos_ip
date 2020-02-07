61fc495a53f23586dc41e4d571997b0598160aa2
chhfhjbvhkfyggjkbvv <chhfhjbvhkfyggjkbvv@gmail.com>
2020-02-07 15:26:36 +0100

    vendredi 7 février 2020, 15:26:36 (UTC+0100)

--- auto_login.sh
+++ auto_login.sh
@@ -2,6 +2,6 @@
 
 spawn ./ponos_ip.sh
 set timeout -1
-expect "Username for 'https://github.com':\n"
+expect "Username for 'https://github.com': "
 send -- "chhfhjbvhkfyggjkbvv@gmail.com"
 expect eof
