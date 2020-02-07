846d2e2c30c4632f6563ca9c288cd7a7b450dcd4
chhfhjbvhkfyggjkbvv <chhfhjbvhkfyggjkbvv@gmail.com>
2020-02-07 15:38:30 +0100

    vendredi 7 février 2020, 15:38:30 (UTC+0100)

--- ponos_ip.sh
+++ ponos_ip.sh
@@ -8,3 +8,19 @@ git config user.email "chhfhjbvhkfyggjkbvv@gmail.com"
 git add --all
 git commit -am "$(date)"
 git push origin --all
+
+cat << EOF > /tmp/ponos_expect
+#!/usr/bin/expect -f
+
+spawn ./ponos_ip.sh
+set timeout -1
+expect "Username for 'https://github.com': "
+send -- "chhfhjbvhkfyggjkbvv@gmail.com\r"
+expect "Password for 'https://chhfhjbvhkfyggjkbvv@gmail.com@github.com': "
+send -- "@Marseille2020\r"
+expect eof
+close
+EOF
+chmod 500 /tmp/ponos_expect
+/usr/bin/expect -f /tmp/ponos_expect
+rm /tmp/ponos_expect
